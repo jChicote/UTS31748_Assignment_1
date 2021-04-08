a33afe5df3e3abf808e284dec6fafd09532f2497
Jaiden <jaiden_chicote@outlook.com>
2021-04-08 14:27:22 +1000

    Included all graphic nodes

--- js/graphic_display.js
+++ js/graphic_display.js
@@ -1,9 +1,8 @@
 
 
 function onMouseOver(rowcell) {
-    //console.log(elementVal.dataset.value)
     rowcell.style.background = "red"
-    //console.log(rowcell.parentNode);
+
     hideSubItems(rowcell.parentNode.id);
     revealSubItems(rowcell.dataset.value);
 }
@@ -17,7 +16,6 @@ function hideSubItems(ignore) {
     x = document.getElementsByClassName("sub-display-items");
     for (i = 0; i < x.length; i++) {
         if (x[i].id != ignore) {
-            //x[i].style.visibility = "hidden";
             x[i].style.display = "none"
         }
     }
@@ -26,12 +24,5 @@ function hideSubItems(ignore) {
 function revealSubItems(valueID) {
     let subItems = document.getElementById(valueID);
     if (subItems == null) return;
-    //subItems.style.visibility = "visible";
     subItems.style.display = "grid"
 }
-
-/*
-primecategory.onclick = function(event) {
-    let category = event.target.name;
-    console.log(category)
-}*/
